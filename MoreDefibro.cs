1cdd75b7ced8ac7bd7cf1b44beb3c7f1d3803cff
TooRed2025 <543400088@qq.com>
2026-06-15 01:36:37 +0800

    1

--- MoreDefibro.cs
+++ MoreDefibro.cs
@@ -13,7 +13,6 @@ namespace MoreDefibro
         internal new static ManualLogSource Logger => Instance.Log;
         private ManualLogSource Log => base.Logger;
         internal Harmony Harmony { get; set; }
-        public static ConfigEntry<bool> EnableMod { get; private set; }
         public static ConfigEntry<int> MinPrice { get; private set; }
         public static ConfigEntry<int> MaxPrice { get; private set; }
         public static ConfigEntry<int> MinSpawnCount { get; private set; }
@@ -24,17 +23,11 @@ namespace MoreDefibro
         {
             Instance = this;
 
-            // Prevent the plugin from being deleted
-            this.gameObject.transform.parent = null;
-            this.gameObject.hideFlags = HideFlags.HideAndDontSave;
             MinSpawnCount = Config.Bind("Settings", "MinSpawnCount", 1, new ConfigDescription("最小生成数量", new AcceptableValueRange<int>(0, 10)));
             MaxSpawnCount = Config.Bind("Settings", "MaxSpawnCount", 4, new ConfigDescription("最大生成数量", new AcceptableValueRange<int>(1, 10)));
             MinPrice = Config.Bind("Settings", "MinPrice", 2, new ConfigDescription("最低价格(千)", new AcceptableValueRange<int>(1, 30)));
             MaxPrice = Config.Bind("Settings", "MaxPrice", 10, new ConfigDescription("最高价格(千)", new AcceptableValueRange<int>(10, 60)));
-            EnableMod = Config.Bind("Settings", "EnableMod", true, new ConfigDescription("启用Mod"));
             DebugMode = Config.Bind("Settings", "DebugMode", false, new ConfigDescription("启用调试日志"));
-
-            EnableMod.SettingChanged += OnEnableModChanged;
             Patch();
 
             Log.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
@@ -46,28 +39,9 @@ namespace MoreDefibro
             Harmony.PatchAll();
         }
 
-        internal void Unpatch()
-        {
-            Harmony?.UnpatchSelf();
-        }
-
         internal static void DeBug(string message)
         {
             if (DebugMode.Value) Logger.LogInfo($"{message}");
-        }
-
-        private void OnEnableModChanged(object sender, System.EventArgs e)
-        {
-            if (EnableMod.Value)
-            {
-                Patch();
-                Log.LogInfo("Mod enabled.");
-            }
-            else
-            {
-                Unpatch();
-                Log.LogInfo("Mod disabled.");
-            }
         }
     }
 }
